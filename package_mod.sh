62ac0e400e8024beae62c7ce2dc7d71a945f9947
lambda-dom <grodrigues.math@gmail.com>
2025-11-25 13:54:22 +0000

    Fill in package_mod values.

--- package_mod.sh
+++ package_mod.sh
@@ -4,9 +4,9 @@
 source "../ModPackaging/utilities/ie_games.sh"
 
 # /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
-export mod_name="Real Mod"
-export mod_version="v4"
-export mod_folder="real_mod"
+export mod_name="Spell Revisions"
+export mod_version="v4.19"
+export mod_folder="spell_rev"
 
 # /* MODIFY: these variables have sensible default values but they may need to be tweaked to match your particular mod */
 # /* the ico_folder is where your .ico files are stored, which is usually the mod's backup or style folder */
@@ -31,5 +31,3 @@ export lowercase_filenames=0
 
 # /* this performs the actual packaging */
 bash "../ModPackaging/utilities/complete_packaging.sh"
-
-
